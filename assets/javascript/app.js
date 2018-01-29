8ff91d3b8443047980ce74bf5f0eadd41a2ebf50
DeegiiAlt <aagiideegii@gmail.com>
2018-01-28 23:32:13 -0700

    added pseudocode for js

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+//Variables set to blank
+	//object with questions and it's answer options.
+	
+//On page load
+
+	//Functions
+		//start game
+			//On-click event for start
+			//for loop for each q
+				//var timeRemaining: 30 sec each q
+				//each q time is decremented by 1000 (1s)
+				//if/else
+					//after time ends or answer selected show image
+					//if correct show correct message
+					//if incorrect show incorrect message
+					//if out of time show out of time message
+					//then on to next question after 5 seconds	
+		//game over
+			//show correct answers, incorrect answers, unaswered q's.
+			//show start over? BUTTON (on-click event)
+			//no reload of page, resets game.
+		//reset
+			//call reset function
+
+
+
